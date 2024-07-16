4e94c98196061cfd0419d9e61516426a662fb0cf
azelisee <108293718+azelisee@users.noreply.github.com>
2024-07-16 19:49:23 +0100

    Create mon_age.py
    Afficher mon age

---
+++ Exos/mon_age.py
@@ -0,0 +1,15 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Ce programme affiche mon age
+>>> print("Affichage de mon age")
+Affichage de mon age
+>>> #Les donnees
+>>> annee0= 2001  #votre annee de naissance
+>>> anneeA= 2021  #Annee actuelle
+>>> print("mon age est:",annee0-anneeA,"ans")
+mon age est: -20 ans
+>>> print("mon age est:",anneeA-annee0,"ans")
+mon age est: 20 ans
+>>> print("Au revoir")
+Au revoir
+>>> 
