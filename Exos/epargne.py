86224a048e5b6cba68ee7a7afc2bef28809f4bff
azelisee <108293718+azelisee@users.noreply.github.com>
2024-07-16 19:45:49 +0100

    Create epargne.py
    Ce script calcule l'épargne cumulée par versements mensuels

---
+++ Exos/epargne.py
@@ -0,0 +1,12 @@
+# Ce script calcule l'épargne cumulée par versements mensuels
+# Utilisation de la boucle for
+
+#Affichage du titre
+print("\n\t", 59* "*")
+print("\t ** Calcul de l'épargne cumulée par versements mensuels **")
+print("\t", 59* "*", "\n")
+
+# Saisie des données
+vp = float(input("\n\t Saisissez la mensualité Vp           :"))
+j = float(input("\n\t Saisissez le taux d'intéret annuel :"))
+a = float(input("\n\t Saisissez le terme en année(s)     :"))
